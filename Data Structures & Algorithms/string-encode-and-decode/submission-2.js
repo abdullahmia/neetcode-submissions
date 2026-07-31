c1bff9339a8b5e1dff91c00b59e7bf7cff595dc9
Abdullah Mia <57964315+abdullahmia@users.noreply.github.com>
2026-08-01 02:58:00 +0600

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        if (!strs.length) return "";
+        let str = "";
+        for (const s of strs) {
+            str += s.length + '#' + s;
+        }
+        return str;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        if (!str.length) return [];
+        let res = [];
+        let i = 0;
+
+        while (i < str.length) {
+            let j = i;
+            while (str[j] != "#") {
+                j++;
+            } 
+            const len = parseInt(str.slice(i, j));
+            res.push(str.slice(j+1, j+1+len));
+            i = j + 1 + len;
+        }
+
+        return res;
+    }
+}
